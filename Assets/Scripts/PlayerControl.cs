1a61b61512a17d68c504bda5c07aff4f06820251
krpopp <kp1640@nyu.edu>
2023-04-26 13:35:44 -0400

    adding animation stuff

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -14,6 +14,8 @@ public class PlayerControl : MonoBehaviour
 
     public float speed;
 
+    public Animator myAnimator;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -29,6 +31,15 @@ public class PlayerControl : MonoBehaviour
     void KeyControl()
     {
         vel = Vector3.zero;
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            speed = 7;
+        }
+        else {
+            speed = 5;
+        }
+
         if (Input.GetKey(upKey))
         {
             vel.z = speed;
@@ -53,6 +64,23 @@ public class PlayerControl : MonoBehaviour
         Quaternion toRotate = Quaternion.LookRotation(rotation.normalized, Vector3.up);
         transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotate, 300 * Time.deltaTime);
         //vel = transform.TransformDirection(vel);
+
+        if (vel == Vector3.zero)
+        {
+            myAnimator.SetBool("isWalking", false);
+        }
+        else
+        {
+            myAnimator.SetBool("isWalking", true);
+            if (speed == 7)
+            {
+                myAnimator.SetBool("isRunning", true);
+            }
+            else
+            {
+                myAnimator.SetBool("isRunning", false);
+            }
+        }
 
         myCharControl.SimpleMove(vel);
 
